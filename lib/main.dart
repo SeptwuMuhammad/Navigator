dc46bb62826c67a2994b3bf7280f33bd293d515d
SeptwuMuhammad <mseptwu@gmail.com>
2022-04-12 23:02:47 +0700

    Navigator

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tugas3/pagesatu.dart';
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: VokasiApp(),
+    );
+  }
+}
